1bbcd5af41a9c0db771bc2dbe6ff682539ee4530
eddie1031 <archaco123@gmail.com>
2026-05-15 16:53:03 +0900

    init

---
+++ src/main/java/io/eddie/formbased/FormBasedApplication.java
@@ -0,0 +1,13 @@
+package io.eddie.formbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBasedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormBasedApplication.class, args);
+    }
+
+}
---
+++ src/main/java/io/eddie/formbased/config/SecurityConfiguration.java
@@ -0,0 +1,71 @@
+package io.eddie.formbased.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .formLogin(Customizer.withDefaults())
+//                .formLogin(formConfig -> formConfig.loginPage("/login-page")
+//                        .usernameParameter("loginId")
+//                        .passwordParameter("loginPwd")
+//                        .successForwardUrl("/")
+//                        .defaultSuccessUrl("/")
+//                        .failureUrl("/login-page?error")
+//                        .failureForwardUrl("/login-page?error")
+//                )
+                .logout(Customizer.withDefaults())
+//                .logout( logoutConfig -> logoutConfig
+//                        .logoutUrl("/logout")
+//                        .invalidateHttpSession(true)
+//                        .deleteCookies("JSESSIONID")
+//                        .clearAuthentication(true)
+//                )
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/login-page")
+//                                .anonymous()
+//                                .permitAll()
+//                                .hasAnyRole()
+//                                .hasAnyAuthority()
+//                                .hasRole()
+//                                .hasAuthority()
+                )
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(User
+                .withUsername("happy")
+                .password(
+                        passwordEncoder.encode("day")
+                )
+                .build()
+        );
+
+        return manager;
+
+    }
+
+
+}
